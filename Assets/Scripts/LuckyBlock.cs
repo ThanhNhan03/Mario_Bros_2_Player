3236a269b0c16988f79ebe7a9c371819fa70fe2e
JOSEE <nhan321hd7e@gmail.com>
2025-02-24 09:42:54 +0700

    Add Lucky Block
    lucky block will include power up, coin and enemy

---
+++ Assets/Scripts/LuckyBlock.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class LuckyBlock : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public GameObject[] spawnItems;
+    public Transform spawnPoint;
+    public AudioClip hitSound;
+
+    private bool isUsed = false;
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!isUsed && (collision.gameObject.CompareTag("Player1") || collision.gameObject.CompareTag("Player2")))
+        {
+            if (IsHitFromBelow(collision))
+            {
+                ActivateBlock();
+            }
+        }
+    }
+
+    private bool IsHitFromBelow(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            if (contact.point.y < transform.position.y)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void ActivateBlock()
+    {
+        isUsed = true;
+
+        animator.SetTrigger("isUsed");
+
+        if (hitSound != null)
+        {
+            AudioSource.PlayClipAtPoint(hitSound, transform.position);
+        }
+
+        if (spawnItems.Length > 0)
+        {
+            GameObject itemToSpawn = spawnItems[Random.Range(0, spawnItems.Length)];
+
+            Vector3 spawnPosition = transform.position + Vector3.up * 1.0f;
+
+            Instantiate(itemToSpawn, spawnPosition, Quaternion.identity);
+        }
+    }
+}
